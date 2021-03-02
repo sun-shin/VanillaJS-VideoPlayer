55e781b5c275558a6aa5f3c7cf33e7583930b58b
Alex Racinski <aracinski2@gmail.com>
2021-03-01 19:49:23 -0600

    make video player dynamic with uploaded file, move widget to file_upload.js

--- javascript/file_upload.js
+++ javascript/file_upload.js
@@ -1,19 +1,15 @@
-// requirejs('dotenv').config();
+const myVideo = document.getElementById("myVideo");
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'sdfajjjadfsk',
+  uploadPreset: 'vf_upload'
+}, (error, result) => {
+  if (!error && result && result.event === "success") {
+    console.log('Done! Here is the image info: ', result.info);
+    myVideo.src = result.info.url;
+  }
+}
+);
 
-// window.ajaxSuccess = function () {
-//   response = JSON.parse(this.responseText);
-//   console.log("ajaxSuccess", typeof this.responseText);
-//   document.getElementById('uploaded').setAttribute("src", response["secure_url"]);
-//   document.getElementById('results').innerText = this.responseText;
-// }
-
-// window.AJAXSubmit = function (formElement) {
-//   console.log("starting AJAXSubmit");
-//   if (!formElement.action) { return; }
-//   var xhr = new XMLHttpRequest();
-//   xhr.onload = ajaxSuccess;
-//   xhr.open("post", "https://api.cloudinary.com/v1_1/<CLOUD_NAME>/video/upload");
-//   xhr.send(new FormData(formElement));
-// }
-
-// console.log(process.env.CLOUD_NAME);
+document.getElementById("upload_widget").addEventListener("click", function () {
+  myWidget.open();
+}, false);
